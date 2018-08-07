3766065c60b6eb1c5b7e34c260caa6f2cb2209b0
raenier <raenierquiambao@gmail.com>
2018-08-07 22:16:16 +0800

    get user by username

--- lib/its/accounts/accounts.ex
+++ lib/its/accounts/accounts.ex
@@ -38,6 +38,17 @@ defmodule Its.Accounts do
   def get_user!(id), do: Repo.get!(User, id)
 
   @doc """
+  Gets single user by username attribute
+
+  """
+  def get_user_by_username(username) when is_nil(username) do
+    nil
+  end
+  def get_user_by_username(username) do
+    Repo.get_by(User, username: username)
+  end
+
+  @doc """
   Creates a user.
 
   ## Examples

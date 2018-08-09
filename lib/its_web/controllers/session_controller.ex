cf343d0b261f7807f7e410b4c7b052a00c194377
raenier <raenierquiambao@gmail.com>
2018-08-09 11:51:29 +0800

    Remove layout on sign-in page

--- lib/its_web/controllers/session_controller.ex
+++ lib/its_web/controllers/session_controller.ex
@@ -4,12 +4,14 @@ defmodule ItsWeb.SessionController do
   alias Its.Accounts
 
   def new(conn, _params) do
+    conn = put_layout conn, false
     render conn, "new.html"
   end
 
   def create(conn, %{"session" => auth_params}) do
     user = Accounts.get_user_by_username(auth_params["username"])
     if is_nil(user) do
+      conn = put_layout conn, false
       render(conn, "new.html")
     else
       if auth_params["password"] == user.password do
@@ -18,7 +20,9 @@ defmodule ItsWeb.SessionController do
         |> put_session(:current_user_id, user.id)
         |> redirect(to: page_path(conn, :index))
       else
-        render conn, "new.html"
+        conn
+        |> put_layout(false)
+        |> render("new.html")
       end
     end
   end
@@ -26,6 +30,7 @@ defmodule ItsWeb.SessionController do
   def delete(conn, _params) do
     conn
     |> delete_session(:current_user_id)
+    |> put_layout(false)
     |> render("new.html")
   end
 end

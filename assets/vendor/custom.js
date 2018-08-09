f2c06b220c801539ad57aa0bd5f384e503efa230
raenier <raenierquiambao@gmail.com>
2018-08-09 22:23:36 +0800

    Edit pages to accomodate create create js function to reveal passwords

---
+++ assets/vendor/custom.js
@@ -0,0 +1,9 @@
+function reveal(id){
+  console.log("hey");
+  var x = document.getElementById(id);
+    if (x.type === "password") {
+        x.type = "text";
+    } else {
+        x.type = "password";
+    }
+}

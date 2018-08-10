c6d1e99ab1fe4780387b1639edac1c1a69181b8d
raenier <raenierquiambao@gmail.com>
2018-08-10 15:59:54 +0800

    Password and Password confirm must match

--- assets/vendor/custom.js
+++ assets/vendor/custom.js
@@ -7,3 +7,17 @@ function reveal(id){
         x.type = "password";
     }
 }
+
+function match() {
+  if (document.getElementById('user_password').value ==
+    document.getElementById('user_password_confirm').value) {
+    document.getElementById('errmessage').style.color = 'green';
+    document.getElementById('errmessage').innerHTML = 'matching';
+    document.getElementById('submit_user').removeAttribute('disabled');
+  }
+  else {
+    document.getElementById('errmessage').style.color = 'red';
+    document.getElementById('errmessage').innerHTML = 'not matching';
+    document.getElementById('submit_user').disabled='disabled';
+  }
+}

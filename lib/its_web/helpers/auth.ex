2a80b029568707b7ee12b0e48903478878ae3368
raenier <raenierquiambao@gmail.com>
2018-08-21 21:49:06 +0800

    Function for checking user type

--- lib/its_web/helpers/auth.ex
+++ lib/its_web/helpers/auth.ex
@@ -5,4 +5,10 @@ defmodule ItsWeb.Helpers.Auth do
     if user_id, do: !!Its.Repo.get(Its.Accounts.User, user_id)
   end
 
+  #use after using signed_in?/1
+  def check_user_type(conn) do
+    user_id = Plug.Conn.get_session(conn, :current_user_id)
+    user = Its.Accounts.get_user!(user_id)
+    user.type
+  end
 end

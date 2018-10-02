03cff22308414e2aec621b8d264cc2361edc4f53
raenier <raenierquiambao@gmail.com>
2018-10-02 15:45:49 +0800

    Fix bugs

--- assets/vendor/custom.js
+++ assets/vendor/custom.js
@@ -23,16 +23,6 @@ function match() {
 
 function update_text_val(self) {
   document.getElementById('progress-text').innerHTML = self.value + '%';
-  if(self.value == "100") {
-    document.getElementById('submit-ticket').innerHTML = "Mark as DONE <i class='fas fa-check'></i>";
-    document.getElementById('submit-ticket').classList.add("btn-success");
-    document.getElementById('submit-ticket').classList.remove("btn-info");
-  }
-  else {
-    document.getElementById('submit-ticket').innerHTML = "Update";
-    document.getElementById('submit-ticket').classList.add("btn-info");
-    document.getElementById('submit-ticket').classList.remove("btn-success");
-  }
 }
 
 $(document).ready(function(){
